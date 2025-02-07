65a870aed6704eb391f0d6dccca86ac03dd62aad
MMPjayarathne <pramudithajayarathna@gmail.com>
2025-02-07 22:17:14 +0530

    feat: Add Global Exception Handler

---
+++ backend/src/main/java/com/interview/user_management_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.interview.user_management_service.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ValidationException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(ValidationException e) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("message", e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleGeneralException(Exception e) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("message", "An unexpected error occurred: " + e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
